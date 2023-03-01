9da19f2274ccb9a362711e96244c7789f57a5441
张涛 <1304356038@qq.com>
2023-03-01 20:35:19 +0800

    修改hellocontroller

--- src/main/java/com/zt/controller/HelloController.java
+++ src/main/java/com/zt/controller/HelloController.java
@@ -8,11 +8,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class HelloController {
     @RequestMapping("/index")
-    public String hello(Model model){
-        model.addAttribute("msg","<h1>adaw</h1>");
-
+    public String index(Model model){
+        model.addAttribute("msg","<h1>zt</h1>");
         return "index";
-
-
     }
 }
