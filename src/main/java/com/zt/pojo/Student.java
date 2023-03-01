77013148e257bcc7c34e7f079dab6774b4ab6c05
张涛 <1304356038@qq.com>
2023-03-01 19:39:47 +0800

    修改Student

--- src/main/java/com/zt/pojo/Student.java
+++ src/main/java/com/zt/pojo/Student.java
@@ -9,22 +9,5 @@ public class Student {
         this.age = age;
     }
 
-    public Student() {
-    }
 
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public int getAge() {
-        return age;
-    }
-
-    public void setAge(int age) {
-        this.age = age;
-    }
 }
