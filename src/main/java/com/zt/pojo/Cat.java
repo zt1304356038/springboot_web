9cdb68acab00cbd74c9ea1b94411b21f91494df1
张涛 <1304356038@qq.com>
2023-03-01 19:11:17 +0800

    添加dao包

---
+++ src/main/java/com/zt/pojo/Cat.java
@@ -0,0 +1,17 @@
+package com.zt.pojo;
+
+public class Cat {
+    public String name;
+
+    public Cat(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
